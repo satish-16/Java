b993ba48c8f088806196a6e326d4f7ef45e09bb4
Satish <satishcy1@gmail.com>
2023-05-02 11:49:21 -0400

    Encapsulation Demo

---
+++ src/Encapsulation/Encapsulation1.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Encapsulation1 {
+    private String myName;
+    public String getName() {
+        return myName;
+    }
+    public void setName(String myName){
+         this.myName = myName;
+    }
+}
---
+++ src/Encapsulation/Encapsulation2.java
@@ -0,0 +1,9 @@
+package Encapsulation;
+
+public class Encapsulation2 {
+    public static void main(String[] args) {
+        Encapsulation1 e1 = new Encapsulation1();
+        e1.setName("Satish-16");
+        System.out.println(e1.getName());
+    }
+}
